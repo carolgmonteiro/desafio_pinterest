65ba609a3b49e2e330f5fcf8df653ae61fbdac18
carolgmonteiro <carolgmonteiro@live.com>
2019-10-22 16:37:26 -0300

    Updates

---
+++ precache-manifest.702d16ac39172c86b9238a7dc8d7d6ae.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d0329045e37fdada21bb7daa398a78",
+    "url": "/desafio_pinterest/index.html"
+  },
+  {
+    "revision": "d829b3b6af3612c12dff",
+    "url": "/desafio_pinterest/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "8d027c39f8f8b2062b20",
+    "url": "/desafio_pinterest/static/css/main.b0c81873.chunk.css"
+  },
+  {
+    "revision": "d829b3b6af3612c12dff",
+    "url": "/desafio_pinterest/static/js/2.287fe4e9.chunk.js"
+  },
+  {
+    "revision": "8d027c39f8f8b2062b20",
+    "url": "/desafio_pinterest/static/js/main.53a951b5.chunk.js"
+  },
+  {
+    "revision": "eb2796d29b7ca49a6123",
+    "url": "/desafio_pinterest/static/js/runtime-main.95011859.js"
+  },
+  {
+    "revision": "9f54134522fc0b9f18763da7cbb73352",
+    "url": "/desafio_pinterest/static/media/arrow-diagonal.9f541345.PNG"
+  },
+  {
+    "revision": "db4c8f7dc76ec3094e040fdb53ccc2da",
+    "url": "/desafio_pinterest/static/media/back.db4c8f7d.svg"
+  },
+  {
+    "revision": "638b8cdccd1220515830c1c30fca4ee4",
+    "url": "/desafio_pinterest/static/media/bell.638b8cdc.svg"
+  },
+  {
+    "revision": "46debb6e448296d29312645065350fde",
+    "url": "/desafio_pinterest/static/media/chat.46debb6e.svg"
+  },
+  {
+    "revision": "b97fda3bce44bc7a6bb4e62c30643055",
+    "url": "/desafio_pinterest/static/media/dots-black.b97fda3b.PNG"
+  },
+  {
+    "revision": "9da64eda994bc02b6ed1658c43b44922",
+    "url": "/desafio_pinterest/static/media/dots.9da64eda.svg"
+  },
+  {
+    "revision": "66a7bb5f9f219e6d6a8808092856194b",
+    "url": "/desafio_pinterest/static/media/logo.66a7bb5f.svg"
+  },
+  {
+    "revision": "83832d7a12bd17356f89f22fa65bb48a",
+    "url": "/desafio_pinterest/static/media/search.83832d7a.svg"
+  },
+  {
+    "revision": "35375e21f1cb1658920edf30054d6ae0",
+    "url": "/desafio_pinterest/static/media/share-black.35375e21.PNG"
+  }
+]);
